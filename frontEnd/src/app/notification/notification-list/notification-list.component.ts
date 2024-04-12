8245057e92e1fb50b8409376b33b0e26724a80cb
Salimlejmi <salimsalem4@gmail.com>
2024-04-12 22:26:29 +0100

    change notification list ui

--- frontEnd/src/app/notification/notification-list/notification-list.component.ts
+++ frontEnd/src/app/notification/notification-list/notification-list.component.ts
@@ -1,4 +1,3 @@
-// notification-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NotificationService } from '../../notification.service';
@@ -10,6 +9,9 @@ import { NotificationService } from '../../notification.service';
 })
 export class NotificationListComponent implements OnInit {
  notifications: any[] = [];
+ currentPage = 1;
+ totalPages = 0;
+ pageSize = 7;
 
  constructor(private router: Router, private notificationService: NotificationService) {}
 
@@ -20,12 +22,39 @@ export class NotificationListComponent implements OnInit {
  loadNotifications(): void {
     this.notificationService.getNotifications().subscribe(response => {
       this.notifications = (response as any).notifications;
+      this.totalPages = Math.ceil(this.notifications.length / this.pageSize);
+      this.paginateNotifications();
     }, error => {
       console.error('Error loading notifications:', error);
     });
  }
 
+ paginateNotifications() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.notifications = [...this.notifications.slice(startIndex, endIndex)];
+ }
+
+ nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateNotifications();
+    }
+ }
+
+ prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateNotifications();
+    }
+ }
+
  navigateToMessage(notification: any): void {
     this.router.navigate(['/contact-message', notification.messageId]);
+ }
+
+ getPhotoUrl(notification: any): string {
+    const baseUrl = 'http://localhost:8000/uploads/';
+    return notification && notification.sender_picture ? baseUrl + notification.sender_picture : '';
  }
 }
