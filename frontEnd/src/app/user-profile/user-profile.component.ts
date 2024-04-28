e680fe1658fed8e129c99b5ee3b8cb922301b1e5
Salimlejmi <salimsalem4@gmail.com>
2024-04-28 12:24:23 +0100

    added user profile

--- frontEnd/src/app/user-profile/user-profile.component.ts
+++ frontEnd/src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service'; // Adjust the path as necessary
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+ selector: 'app-user-profile',
+ templateUrl: './user-profile.component.html',
+ styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+ username: string = '';
+ pictureUrl: string | null = null;
+ roles: string[] = [];
 
+ constructor(private authService: AuthService) {}
+
+ ngOnInit(): void {
+    this.username = this.authService.getUsername();
+     this.roles = this.authService.getRoles();
+ }
 }
